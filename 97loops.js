42500ebae19ed086f3c12f55117e23db27e7c8c7
MathurMohit2808 <mm1485176@gmail.com>
2025-06-26 15:05:25 +0530

    done

--- 97loops.js
+++ 97loops.js
@@ -18,3 +18,43 @@ for(let value of arr)
 {
     console.log(value)
 }
+
+//now we will learn forEach method -> it modifies the original array -> doe snot have any return statement
+
+let a1 = [1,2,3,4,5,6,7,8];
+
+a1.forEach((value,index,array) => {
+    array[index] = value + 2;
+});
+
+console.log(a1);
+
+//Now, map
+// this does not modify original array
+// it has return statement in it
+
+const array1 = a1.map((value,index) => {
+    return value + 2;
+});
+
+console.log(array1);
+
+//filter
+// same characters as map
+
+const array4 = a1.filter((value,index) => {
+    return value % 2 === 0;
+});
+
+console.log(array4);
+
+//find
+// diff between filter and find is that 
+// filter gives all the elements which satisfies the condition
+// whereas find only return the first find
+
+const finded = a1.find((value,index) => {
+    return value % 2 === 0;
+});
+
+console.log(finded);                               
